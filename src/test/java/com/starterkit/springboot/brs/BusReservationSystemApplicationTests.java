a8626538b1cebdcf27d296151214f9b99c5163ab
SouElk <108268090+SouElk@users.noreply.github.com>
2022-07-01 12:08:43 +0200

    Update BusReservationSystemApplicationTests.java

--- src/test/java/com/starterkit/springboot/brs/BusReservationSystemApplicationTests.java
+++ src/test/java/com/starterkit/springboot/brs/BusReservationSystemApplicationTests.java
@@ -6,9 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 @RunWith(SpringRunner.class)
